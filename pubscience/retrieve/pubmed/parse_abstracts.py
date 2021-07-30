a5dea1f59439d30ab8f3634448c7f067adc34aa7
Bram <bramiozo@gmail.com>
2021-07-30 10:44:56 +0200

    update of parsers

--- pubscience/retrieve/pubmed/parse_abstracts.py
+++ pubscience/retrieve/pubmed/parse_abstracts.py
@@ -11,40 +11,44 @@ import argparse
 # TODO: use --folder/out/[filename].raw.txt or as output location unless specified otherwise with --outfolder
 # TODO: use [root] of filename for [root]/out/[filename].raw.txt as output location if 
 #       --folder and --outfolder not given 
-filename = sys.argv[1]
-rawout = filename+".abstract.raw"
+dir_name = sys.argv[1]
+files = os.listdir(dir_name)
+files = [f for f in files if '.xml' in f]
 
-with open(filename) as rf:
-    xmls = [line.strip() for line in rf if len(line.strip())>0]
-    xmls = "\r".join(xmls)
-    
-# split by </xml>
-xmls_arr = xmls.split("<?xml version=\"1.0\" ?>")[1:]
-del xmls
-gc.collect()
+for filename in files:
+    rawout = os.path.join(dir_name, 'out', filename+".abstract.raw")
 
-print(f"There are {len(xmls_arr)} XML objects in {filename}")
-
-with open(rawout, 'w', encoding='utf-8') as wf:
-    while xmls_arr:
-        _xml = xmls_arr.pop()
-        xml_str = re.sub(r"<\?xml[^><]+\?>", "", _xml).strip()
-        xml_str = re.sub(r"<\!DOC[^><]+>\r?<PubmedArticleSet>", "<PubmedArticleSet>", xml_str).strip()
-        xml_str = "<xml>"+xml_str+"</xml>"
-        xml_str = re.sub(r'<\/xml>{2,}', '</xml>', xml_str)
-        xml_str = re.sub(r'<\/xml><\/xml>+', '</xml>', xml_str)
-        xml_str = re.sub(r'>>', '>', xml_str)
-
-        try:
-            root = ET.fromstring(xml_str.encode('utf-8'))
-
-            for idx, ab in tqdm(enumerate(root.iter('AbstractText'))):
-                wf.write(ab.text+"\n")
+    with open(os.path.join(dir_name, filename), encoding='latin1') as rf:
+        xmls = [line.strip() for line in rf if len(line.strip())>0]
+        xmls = "\r".join(xmls)
         
-            print(f"Number of abstracts in this chunk: {idx}")
-        except Exception as e:
-            print(f"XML processing failed with error; {e}")
-            print("first characters:")
-            print(xml_str[:100])
-            print("last characters:")
-            print(xml_str[-100:])
+    # split by </xml>
+    xmls_arr = xmls.split("<?xml version=\"1.0\" ?>")[1:]
+    del xmls
+    gc.collect()
+
+    print(f"There are {len(xmls_arr)} XML objects in {filename}")
+
+    with open(rawout, 'w', encoding='utf-8') as wf:
+        while xmls_arr:
+            _xml = xmls_arr.pop()
+            xml_str = re.sub(r"<\?xml[^><]+\?>", "", _xml).strip()
+            xml_str = re.sub(r"<\!DOC[^><]+>\r?<PubmedArticleSet>", "<PubmedArticleSet>", xml_str).strip()
+            xml_str = "<xml>"+xml_str+"</xml>"
+            xml_str = re.sub(r'<\/xml>{2,}', '</xml>', xml_str)
+            xml_str = re.sub(r'<\/xml><\/xml>+', '</xml>', xml_str)
+            xml_str = re.sub(r'>>', '>', xml_str)
+
+            try:
+                root = ET.fromstring(xml_str.encode('latin1'))
+
+                for idx, ab in tqdm(enumerate(root.iter('AbstractText'))):
+                    wf.write(ab.text+"\n")
+            
+                print(f"Number of abstracts in this chunk: {idx}")
+            except Exception as e:
+                print(f"XML processing failed with error; {e}")
+                print("first characters:")
+                print(xml_str[:100])
+                print("last characters:")
+                print(xml_str[-100:])
--- pubscience/retrieve/pubmed/parse_fulltext.py
+++ pubscience/retrieve/pubmed/parse_fulltext.py
@@ -5,40 +5,60 @@ import re
 import gc
 from tqdm import tqdm
 
-filename = sys.argv[1]
-rawout = filename+".abstract.raw"
+dir_name = sys.argv[1]
+files = os.listdir(dir_name)
+files = [f for f in files if '.xml' in f]
 
-with open(filename) as rf:
-    xmls = [line.strip() for line in rf if len(line.strip())>0]
-    xmls = "\r".join(xmls)
-    
-# split by </xml>
-xmls_arr = xmls.split("<?xml version=\"1.0\" ?>")[1:]
-del xmls
-gc.collect()
+for filename in files:
+    rawout = os.path.join(dir_name, 'out', filename+".full.raw")
 
-print(f"There are {len(xmls_arr)} XML objects in {filename}")
+    with open(os.path.join(dir_name, filename), encoding='latin1') as rf:
+        xmls = [line.strip() for line in rf if len(line.strip())>0]
+        xmls = "\r".join(xmls)
 
-with open(rawout, 'w', encoding='utf-8') as wf:
-    while xmls_arr:
-        _xml = xmls_arr.pop()
-        xml_str = re.sub(r"<\?xml[^><]+\?>", "", _xml).strip()
-        xml_str = re.sub(r"<\!DOC[^><]+>\r?<PubmedArticleSet>", "<PubmedArticleSet>", xml_str).strip()
-        xml_str = "<xml>"+xml_str+"</xml>"
-        xml_str = re.sub(r'<\/xml>{2,}', '</xml>', xml_str)
-        xml_str = re.sub(r'<\/xml><\/xml>+', '</xml>', xml_str)
-        xml_str = re.sub(r'>>', '>', xml_str)
 
-        try:
-            root = ET.fromstring(xml_str.encode('utf-8'))
+    # find LAST </article> BEFORE next <\?xml
+    # remove/ignore everything after </article> and before <\?xml
 
-            for idx, ab in tqdm(enumerate(root.iter('AbstractText'))):
-                wf.write(ab.text+"\n")
-        
-            print(f"Number of abstracts in this chunk: {idx}")
-        except Exception as e:
-            print(f"XML processing failed with error; {e}")
-            print("first characters:")
-            print(xml_str[:100])
-            print("last characters:")
-            print(xml_str[-100:])
+
+
+    # append </pmc-articleset>
+
+    # get all text within <body></body> with Beautifulsoup
+
+    # remove all LaTex
+
+    # continue, clean everything before <\?xml..
+
+            
+
+    xmls_arr = xmls.split("<?xml version=\"1.0\" ?>")[1:]
+    del xmls
+    gc.collect()
+
+    print(f"There are {len(xmls_arr)} XML objects in {filename}")
+
+    with open(rawout, 'w', encoding='utf-8') as wf:
+        while xmls_arr:
+            _xml = xmls_arr.pop()
+            xml_str = re.sub(r"<\?xml[^><]+\?>", "", _xml).strip()
+            xml_str = re.sub(r"<\!DOCTYPE.*>", "", xml_str).strip()
+            xml_str = re.sub(r"<\!DOC[^><]+>\r?<PubmedArticleSet>", "<PubmedArticleSet>", xml_str).strip()
+            xml_str = "<xml>"+xml_str+"</xml>"
+            xml_str = re.sub(r'<\/xml>{2,}', '</xml>', xml_str)
+            xml_str = re.sub(r'<\/xml><\/xml>+', '</xml>', xml_str)
+            xml_str = re.sub(r'>>', '>', xml_str)
+
+            try:
+                root = ET.fromstring(xml_str.encode('latin1'))
+
+                for idx, ab in tqdm(enumerate(root.iter('AbstractText'))):
+                    wf.write(ab.text+"\n")
+            
+                print(f"Number of abstracts in this chunk: {idx}")
+            except Exception as e:
+                print(f"XML processing failed with error; {e}")
+                print("first characters:")
+                print(xml_str[:100])
+                print("last characters:")
+                print(xml_str[-100:])
