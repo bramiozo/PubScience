78b6a9ef81ea8764b2ed0adaddee4c1185d36fe5
Bram <bramiozo@gmail.com>
2021-10-25 13:20:18 +0200

    added OAI's

--- pubscience/retrieve/OAI/oai.py
+++ pubscience/retrieve/OAI/oai.py
@@ -2,8 +2,6 @@ import requests
 import re
 import xml
 
-# Radboud - fulltext not available?
-# 
 
 YYYY="2010"
 sources = {
@@ -20,9 +18,20 @@ sources = {
                 'date': 'date', 
                 'subject': 'subject',
                 'type': 'type'},
-    'UTwente':{},
-    'TUD':{},
-    'RUG': {},
+    'UTwente':{'link': f'https://ris.utwente.nl/ws/oai?metadataPrefix=oai_dc&verb=ListRecords&set=publications:{YYYY}:withFiles',
+               'pdf': 'identifier',
+               'follow': 'direct',
+               'lang': 'language',
+               'date': 'date',
+               'subject': 'subject',
+               'type': 'type'}
+    'RUG': {'link': f'https://pure.rug.nl/ws/oai?metadataPrefix=oai_dc&verb=ListRecords&set=publications:year{YYYY}:withFiles',
+            'pdf': 'identifier',
+            'follow': 'direct',
+            'lang': 'language',
+            'date': 'date', 
+            'type': 'type'
+            },
     'Maastricht':{'link': f'https://cris.maastrichtuniversity.nl/ws/oai?verb=ListRecords&metadataPrefix=oai_dc&set=publications:year{YYYY}:withFiles', 
                 'pdf': 'identifier',
                 'follow': 'direct',
@@ -53,5 +62,36 @@ sources = {
                 'language': 'dc:language',
                 'type': 'dc:type'
              },
-    'Narcis':
+    'VU': {'link': f'https://research.vu.nl/ws/oai?metadataPrefix=oai_dc&verb=ListRecords&set=publications:year{YYYY}:withFiles'
+           'pdf': 'identifier',
+           'follow': 'direct',
+           'lang': 'language',
+           'type': 'type',
+           'date': 'date',
+           'thesis_set': f'studenttheses:year{YYYY}:withFiles'
+           },
+    'Leiden': {'link': f'https://scholarlypublications.universiteitleiden.nl/oai2?metadataPrefix=oai_dc&verb=ListRecords&set={SET}',
+              'pdf': 'dc:identifier',
+              'follow': 'retrieve',
+              'language': 'dc:language',
+              'type': 'dc:type',
+              'set': ['hdl_1887_55785', 'hdl_1887_4540', 'hdl_1887_9744', 'hdl_1887_85175', 'hdl_1887_20801', 'open_access'],
+              'inclusion': {'language': 'nl', 'type': 'Doctoral Thesis'
+    },
+    'Erasmus':{},
+    'TUDelft':{},
+    'Radboud':{'link':f'https://repository.ubn.ru.nl/oai/request?verb=ListRecords&metadataPrefix=oai_dc&set={SET}',
+               'pdf': 'dc:identifier',
+               'follow': 'retrieve',
+               'type': 'dc:type',
+               'set': ['col_2066_119636', 'col_2066_119645', 'com_2066_13798']
+             },
+    'KNAW': {'link': f'https://pure.knaw.nl/ws/oai?metadataPrefix=oai_dc&verb=ListRecords&set={SET}',
+             'pdf': 'identifier',
+             'follow': 'direct',
+             'language': 'language',
+             'type': 'type',
+             'set': ['openaire_cris_publications', f'publications:year{YYYY}:withFiles'],
+             'inclusion': {'language': 'nl', 'type': 'Doctoral Thesis'}
+             }
 }
