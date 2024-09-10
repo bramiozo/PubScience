bb42e3231558e607d4e068ef3842bedd118e5c9a
Bram van Es <bramiozo@gmail.com>
2024-09-10 20:48:18 +0200

    Working on llms

--- pubscience/translate/llm.py
+++ pubscience/translate/llm.py
@@ -2,3 +2,33 @@
 This module contains classes to translate annotated and non-annotated corpora from
 a source language to a target language.
 '''
+
+import os
+from dotenv import load_dotenv
+
+import google.generativeai as google_gen
+from anthropic import Client as anthropic_client
+from openai import Client as openai_client
+
+from typing import Optional, Dict, List, Any
+
+load_dotenv('.env')
+
+
+
+class TranslationLLM:
+    def __init__(self,
+        api_key: str,
+        model: str,
+        engine: str):
+
+        self.api_key = api_key
+        self.model = model
+        self.engine = engine
+
+        if engine == 'openai':
+            self.client = openai_client(api_key=os.getenv('OPENAI_LLM_API_KEY'))
+        elif engine == 'anthropic':
+            self.client = anthropic_client(api_key=os.getenv('ANTHROPIC_LLM_API_KEY'))
+        elif engine == 'google':
+            self.client = google_gen.Client(api_key=os.getenv('GOOGLE_LLM_API_KEY'))
