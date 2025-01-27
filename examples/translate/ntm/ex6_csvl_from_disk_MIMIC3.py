ae3a0ce5586c81d316ac86a6c23f6d141200fcb8
Bram van Es <bramiozo@gmail.com>
2025-01-27 17:28:14 +0100

    upd

--- examples/translate/ntm/ex6_csvl_from_disk_MIMIC3.py
+++ examples/translate/ntm/ex6_csvl_from_disk_MIMIC3.py
@@ -7,13 +7,13 @@ import pandas as pd
 import re # for removing repeated underscores
 from pubscience.translate import ntm
 
-dotenv.load_dotenv('.env')
+dotenv.load_dotenv('../../.env')
 csv_example_dir = os.getenv('MIMIC3_folder')
 
 # list of file
 #
 file_list = os.listdir(csv_example_dir)
-BATCH_SIZE = 32
+BATCH_SIZE = 3
 USE_GPU = True
 TEXT_ID = 'TEXTS'
 ID_COL = 'id'
@@ -25,16 +25,16 @@ DEID_NORMALISATION_REGEX = {
     'DATE_1':               (r'\[\*\*[0-9]{4,5}\-[0-9]{1,2}\-[0-9]{1,2}\*\*\]', '[DATE]'),
     'DATE_2':               (r'\[\*\*Month \(only\) [0-9]{1,4}\*\*\]', '[DATE]'),
     'PATIENT_NAME_1':       (r'\[\*\*Name[0-9]{1,4} \(NameIs\)\s[0-9]{0,4}\*\*\]', '[PATIENT_NAME]'),
-    'PATIENT_NAME_2':       (r'\[\*\*Known firstname [0-9]{1,5}\*\*\]', '[PATIENT_FIRST_NAME]'),
-    'PATIENT_NAME_3':       (r'\[\*\*Known lastname [0-9]{1,5}\*\*\]', '[PATIENT_LAST_NAME]'),
-    'FIRST_NAME_1':         (r'\[\*\*First Name[0,9]{0,5} \(LF\)\s\*\*\]', '[FIRST_NAME]'),
-    'FIRST_NAME_2':         (r'\[\*\*First Name[0,9]{0,5} \(Titles\)\s\*\*\]', '[FIRST_NAME]'),
-    'FIRST_NAME_3':         (r'\[\*\*First Name[0,9]{0,5} \(NamePattern[0-9]{0,5}\)\s\*\*\]', '[FIRST_NAME]'),
-    'FIRST_NAME_4':         (r'\[\*\*First Name[0,9]{0,5} \(NamePattern[0-9]{0,5}\)\s[0-9]{1,4}\*\*\]', '[FIRST_NAME]'),
-    'LAST_NAME_1':          (r'\[\*\*Last Name[0,9]{0,5} \(STitle\)\s\*\*\]', '[LAST_NAME]'),
-    'LAST_NAME_2':          (r'\[\*\*Last Name[0,9]{0,5} \(Titles\)\s\*\*\]', '[LAST_NAME]'),
-    'LAST_NAME_3':          (r'\[\*50\*Last Name[0,9]{0,5} \(NamePattern[0-9]{0,5}\)\s\*\*\]', '[LAST_NAME]'),
-    'LAST_NAME_4':          (r'\[\*\*Last Name[0,9]{0,5} \(NamePattern[0-9]{0,5}\)\s[0-9]{1,4}\*\*\]', '[LAST_NAME]'),
+    'PATIENT_NAME_2':       (r'\[\*\*Known firstname [0-9]{1,6}\*\*\]', '[PATIENT_FIRST_NAME]'),
+    'PATIENT_NAME_3':       (r'\[\*\*Known lastname [0-9]{1,6}\*\*\]', '[PATIENT_LAST_NAME]'),
+    'FIRST_NAME_1':         (r'\[\*\*First Name[0,9]{0,6} \(LF\)\s\*\*\]', '[FIRST_NAME]'),
+    'FIRST_NAME_2':         (r'\[\*\*First Name[0,9]{0,6} \(Titles\)\s\*\*\]', '[FIRST_NAME]'),
+    'FIRST_NAME_3':         (r'\[\*\*First Name[0,9]{0,6} \(NamePattern[0-9]{0,6}\)\s\*\*\]', '[FIRST_NAME]'),
+    'FIRST_NAME_4':         (r'\[\*\*First Name[0,9]{0,6} \(NamePattern[0-9]{0,6}\)\s[0-9]{1,4}\*\*\]', '[FIRST_NAME]'),
+    'LAST_NAME_1':          (r'\[\*\*Last Name[0,9]{0,6} \(STitle\)\s\*\*\]', '[LAST_NAME]'),
+    'LAST_NAME_2':          (r'\[\*\*Last Name[0,9]{0,6} \(Titles\)\s\*\*\]', '[LAST_NAME]'),
+    'LAST_NAME_3':          (r'\[\*50\*Last Name[0,9]{0,6} \(NamePattern[0-9]{0,6}\)\s\*\*\]', '[LAST_NAME]'),
+    'LAST_NAME_4':          (r'\[\*\*Last Name[0,9]{0,6} \(NamePattern[0-9]{0,6}\)\s[0-9]{1,4}\*\*\]', '[LAST_NAME]'),
     'LAST_NAME_5':          (r'\[\*\*Known lastname [0,9]{0,5}\*\*\]', '[LAST_NAME]'),
     'DOCTOR_ID':            (r'\[\*\*MD Number([0,9]{0,3}) [0-9]{1,4}\*\*\]', '[DOCTOR_ID]'),
     'HOSPITAL_1':           (r'\[\*\*Hospital[0-9]{0,2} [0-9]{1,4}\*\*\]', '[HOSPITAL]'),
@@ -45,11 +45,13 @@ DEID_NORMALISATION_REGEX = {
     'DOCTOR_LAST_NAME_1':   (r'\[\*\*Doctor Last Name \*\*\]', '[DOCTOR_LAST_NAME]'),
     'DOCTOR_FIRST_NAME_1':  (r'\[\*\*Doctor First Name \*\*\]', '[DOCTOR_FIRST_NAME]'),
     'MEASUREMENT_VALUE_1':  (r'\[\*\*[0-9]{1,2}\-[0-9]{1,2}\*\*\]', '[DETAIL]'),
-    'MEASUREMENT_VALUE_2':  (r'\[\*\*[0-9]{1,5}\*\*\]', '[MEAS_VALUE]'),
-    'PHONENUMBER_1':        (r'\[\*\*Telephone/Fax \([0-9]{1,2}\) [0-9]{3,4}\*\*\]', '[PHONENUMBER]'),
-    'ADDRESS':              (r'\[\*\*Apartment Address\([0-9]{1,2}\) [0-9]{1,4}\*\*\]', '[ADDRESS]'),
+    'MEASUREMENT_VALUE_2':  (r'\[\*\*[0-9]{1,6}\*\*\]', '[MEAS_VALUE]'),
+    'PHONENUMBER_1':        (r'\[\*\*Telephone/Fax \([0-9]{1,2}\) [0-9]{3,6}\*\*\]', '[PHONE_NUMBER]'),
+    'ADDRESS':              (r'\[\*\*Apartment Address\([0-9]{1,2}\) [0-9]{1,6}\*\*\]', '[ADDRESS]'),
     'IDENTIFIER_1':         (r'\[\*\*Numeric Identifier [0-9]{1,4}\*\*\]', '[IDENTIFIER]'),
     'IDENTIFIER_2':         (r'\[\*\*Job Number [0-9]{1,6}\*\*\]', '[IDENTIFIER]'),
+    'IDENTIFIER_3':         (r'\[\*\*Clip Number [0-9]{1,6}\*\*\]', '[IDENTIFIER]'),
+    'IDENTIFIER_4':         (r'\[\*\*Clip Number (Radiologie) [0-9]{1,6}\*\*\]', '[IDENTIFIER]'),
     'SPURIOUS_REP_1':       (r'\b(\w+)(?:\s+\1\b)+', r'\1'),
     'SPURIOUS_REP_2':       (r'(\W)\1+', r'\1'),
 }
@@ -60,7 +62,10 @@ DEID_NORMALISATION_REGEX = {k: (re.compile(v[0]), v[1]) for k, v in DEID_NORMALI
 # load translation model
 # single: 'vvn/en-to-dutch-marianmt'
 # multi: 'facebook/nllb-200-distilled-600M'
-translator = ntm.TranslationNTM(model_name='vvn/en-to-dutch-marianmt', multilingual=False,
+# multi: 
+
+print("LOADING MODEL...")
+translator = ntm.TranslationNTM(model_name='facebook/nllb-200-distilled-600M', multilingual=True,
                 max_length=MAX_LENGTH, use_gpu=USE_GPU, target_lang='nld_Latn')
 
 for file in file_list:
@@ -69,7 +74,7 @@ for file in file_list:
     name = file.split('.')[0]
     text_df = pd.read_csv(os.path.join(csv_example_dir, file), sep=",", encoding='latin1')
 
-    OUTPUT_LOC = os.path.join(os.getenv('ex6_output_folder'), f"{name}.jsonl")
+    OUTPUT_LOC = os.path.join(os.getenv('MIMIC3_output'), f"{name}.jsonl")
     print(f"Output location: {OUTPUT_LOC}")
 
     MAX_NUM_LINES = text_df.shape[0]
@@ -120,7 +125,7 @@ for file in file_list:
                 if LONG_TEXTS:
                     if batch_size>1:
                         translated_batch = translator.translate_long_batch(batch,
-                            batch_size=24)
+                            batch_size=12)
                     else:
                         translated_batch = [translator.translate_long(batch[0])]
                 else:
