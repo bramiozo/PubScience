f0153df414d08410920bbba41e167391dab38c6a
bramiozo <bramiozo@gmail.com>
2025-06-25 14:45:48 +0200

    changed write folder for pubmed translation

--- examples/translate/ntm/bash_run_ex12.sh
+++ examples/translate/ntm/bash_run_ex12.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /home/bvanes/.cache/pypoetry/virtualenvs/pubscience-F-Cpi52l-py3.10/bin/activate
-python ex12_pubmed.py --output-dir=/home/bvanes/data/pubmed_translations --temp-dir=/home/bvanes/data/temp --batch-size=8 --model=vvn/en-to-dutch-marianmt --max-length=256 --multi_gpu
+accelerate launch --multi_gpu --num_processes=2 /home/bvanes/PubScience/examples/translate/ntm/ex12_pubmed.py --output-dir=/home/bvanes/data/pubmed_pmc/pubmed_translations --temp-dir=/home/bvanes/data/temp --batch-size=24 --model=vvn/en-to-dutch-marianmt --max-length=256 --multi_gpu
